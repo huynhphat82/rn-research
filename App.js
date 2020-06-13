860c4a4a9802dbf745aefb99f7cb590ba0b2324c
huynh phat <huynhphat@huynhs-MacBook-Air.local>
2020-06-13 22:47:04 +0700

    Add firebase, set env & app icon

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import Middleware from '@app/middlewares/Middleware';
 import Test from '@app/components/Test';
+import database from '@react-native-firebase/database';
 
 const App = () => {
+  database().ref('users/123').once('value', snap => console.log(snap.val()));
   return (
     <>
       <SafeAreaView>
